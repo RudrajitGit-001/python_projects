f5b578de689c8ae977d4f9795fdc661ce0a26f9b
Rudrajit Dey <officialrudrajitguwahati@gmail.com>
2026-06-14 18:55:51 +0530

    Changes made in time.py

--- time.py
+++ time.py
@@ -1,18 +1,21 @@
 import time
 my_time=int(input("Enter the time in seconds:"))
 
-#for x in range(my_time,0,-1):
-#    second= x%60
-#    minute=int(x/60)%60
-#    print(f"00:{minute:02}:{second:02}")
-#    time.sleep(1)
-#print("TIMES UP!")
-
-x=my_time
-while x>0:
+for x in range(my_time,0,-1):
     second= x%60
-    minute= int(x/60)%60
-    print(f"00:{minute}:{second}")
+    minute=int(x/60)%60
+    hour=int(x/3600)
+
+    print(f"{hour:02}:{minute:02}:{second:02}")
     time.sleep(1)
-    x-=1
+
 print("TIMES UP!")
+
+#x=my_time
+#while x>0:
+#    second= x%60
+#     minute= int(x/60)%60
+#     print(f"00:{minute}:{second}")
+#     time.sleep(1)
+#     x-=1
+#print("TIMES UP!")
